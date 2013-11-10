6628bec37f2b7e7099a7403d3181cb10041ecdc2
eminaz <qdmark@gmail.com>
2013-11-10 15:40:24 +0800

    get rid of the song, make the volume change according to the brush size

--- public/js/style.js
+++ public/js/style.js
@@ -42,7 +42,7 @@ $(function(){
   });
 
   //Audio
-  var audio = new Audio('musics/01.mp3');
+  //var audio = new Audio('musics/01.mp3');
 
   //canvas
   var canvas = document.getElementById('myCanvas');
@@ -64,7 +64,7 @@ $(function(){
       ctx.strokeStyle =  $(this).attr("class");
 
       //change music
-      audio = new Audio('musics/01.mp3');
+      //audio = new Audio('musics/01.mp3');
 
   });
 
@@ -184,7 +184,7 @@ var saved_dataURL;
      var prev_state = ctx.getImageData(0,0,canvas.width,canvas.height);
       history.pushState(prev_state, null);
      // console.log(prev_state);
-     audio.pause();
+     //audio.pause();
   }
 
   function drawing(e){
@@ -213,7 +213,7 @@ var saved_dataURL;
       e.pageX - canvas.offsetLeft,
       e.pageY - canvas.offsetTop
     );
-    audio.play();
+    //audio.play();
   }
 
 
